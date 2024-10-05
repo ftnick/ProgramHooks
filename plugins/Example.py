e81529bcf8d49de386586368d109261314de29b7
nick <153327552+ftnick@users.noreply.github.com>
2024-10-06 00:01:57 +1000

    Create Example.py
    Upload example plugin which is what the example script needs to.. well be an example

---
+++ plugins/Example.py
@@ -0,0 +1,11 @@
+def pre_init():
+    print("ExamplePlugin - Pre-Init Hook")
+
+def post_init():
+    print("ExamplePlugin - Post-Runtime Hook")
+
+def pre_runtime():
+    print("ExamplePlugin - Pre-Runtime Hook")
+
+def post_runtime():
+    print("ExamplePlugin - Post-Runtime Hook")
